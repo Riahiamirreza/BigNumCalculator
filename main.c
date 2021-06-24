9370d2b561f60c896dd4da1e854e554f2f0ff424
Amirreza <amirreza@localhost.localdomain>
2021-06-25 01:02:28 +0430

    Adding  operation.

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operations.c"
+
+
+
+int main(int argc, char **argv){
+
+	uint8_t a[3] = {253, 5, 2};
+	uint8_t b[3] = {5, 3, 4};
+	uint8_t c[3];
+	addition(3, a,b,c);
+	for(int i=0; i<3; i++)
+		printf("%d-", c[i]);
+
+	fputc('\n', stdout);
+	return 0;
+}
---
+++ operations.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+void addition(int len, uint8_t *a, uint8_t *b, uint8_t *c){
+
+	uint8_t tmp_a, tmp_b;
+	bool ca, cb;
+	bool carry = 0;
+	int i;
+	for(i=0; i<len; i++){
+		
+		c[i] = carry;
+		uint8_t s = a[i] + b[i];
+
+		if(s<a[i] || s<b[i]){
+			puts("here");
+			carry = 1;
+		}else{
+			carry = 0;
+		}
+		c[i] += a[i] + b[i];
+			
+	}
+	c[i] = carry;
+}
+
+
