da2a05f3690d30215b8d22d5fde91a3504089fa8
Amirreza <amirreza@localhost.localdomain>
2021-06-27 13:44:29 +0430

    str to integer added.

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "operations.c"
-
+#include <string.h>
+#include "str.c"
 
 
 int main(int argc, char **argv){
 
-	uint8_t a[3] = {3, 3, 6};
-	uint8_t b[3] = {5, 3, 4};
-	uint8_t c[3];
-	//addition(3, a,b,c);
-	subtraction(3, a,b,c);
-	for(int i=0; i<3; i++)
+	uint8_t c[10];
+	int *t;
+	int g = 0;
+	t = &g;
+	str2int(strlen(argv[1]), argv[1], t, c);
+	for(int i=0; i<*t; i++)
 		printf("%d-", c[i]);
-
 	fputc('\n', stdout);
 	return 0;
 }
--- operations.c
+++ operations.c
@@ -23,6 +23,25 @@ void addition(int len, uint8_t *a, uint8_t *b, uint8_t *c){
 	c[i] = carry;
 }
 
+/*
+ * return 1  if a>b
+ * return 0  if a=b
+ * return -1 if a<b
+ */
+int compare(int a_len, uint8_t *a, int b_len, uint8_t *b){
+
+	if(a_len != b_len)
+		return (a_len > b_len) ? a_len : b_len;
+	int i;
+	for(i=a_len-1; i>=0; i--){
+		if(a[i]>b[i])
+			return 1;
+		else if(a[i]<b[i])
+			return -1;
+	}
+	return 0;
+}
+
 
 void subtraction(int len, uint8_t *a, uint8_t *b, uint8_t *c){
 	
@@ -34,5 +53,14 @@ void subtraction(int len, uint8_t *a, uint8_t *b, uint8_t *c){
 		carry = (a[i]-carry<b[i]);
 		
 	}
+
+}
+
+void mul_32_by_32(uint8_t a, uint8_t b, uint8_t *p1, uint8_t *p2){
+
+	uint16_t mul;
+	mul = (uint16_t)a*b;
+	*p1 = (uint8_t)(mul >> 8);// 8 more signifant bits stores in p1.
+	*p2 = (uint8_t)mul;	  // other other bits stores in p2.
 
 }
